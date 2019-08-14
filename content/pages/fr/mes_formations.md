6d64fe235089637ffd66e65b85597bf4f8557f24
Mathieu Depetris <mathieu.depetris@ird.fr>
2019-08-14 17:47:44 +0200

    add content

---
+++ content/pages/fr/mes_formations.md
@@ -0,0 +1,22 @@
+title: Mes formations
+order: 1
+icon: icon-point-of-interest
+summary: Mes formations.
+slug: education
+date: 2018-08-14
+lang: fr
+
+# Formations principales
+
+<p>
+<img src="/pictures/logos/logo_universite_montpellier.png">
+Master <strong>M</strong>éthodes <strong>S</strong>tatistiques des <strong>I</strong>ndustries <strong>A</strong>gronomiques, <strong>A</strong>groalimentaires et <strong>P</strong>harmaceutiques Anciennement <strong>MSIAAP</strong> maintenant <a href="https://formations.umontpellier.fr/fr/formations/sciences-technologies-sante-STS/master-XB/master-mathematiques-program-fruai0342321nprme157/statistique-pour-les-sciences-de-la-vie-subprogram-pr496.html" target="_blank">SSV</a>, Université de Montpellier – <strong>2013</strong>
+</p>
+
+Master **B**ioressources **A**quatiques en **E**nvironnement **M**éditerranéen et **T**ropical  
+[BAEMT][], Université de Montpellier – **2012**
+
+Master **B**ioressources **A**quatiques en **E**nvironnement **M**éditerranéen et **T**ropical  
+[BAEMT][], Université de Montpellier – **2012**
+
+[BAEMT]: https://biologie-ecologie.com/master-b2e/parcours/baemt/
--- pelicanconf.py
+++ pelicanconf.py
@@ -5,12 +5,17 @@ from __future__ import unicode_literals
 AUTHOR = 'Mathieu Depetris'
 SITENAME = 'MathieuDepetris'
 SITEURL = ''
-
-PATH = 'content'
-
 TIMEZONE = 'Europe/Paris'
-
+DEFAULT_DATE_FORMAT = '%a, %d %b %Y'
 DEFAULT_LANG = 'fr'
+LOCALE = 'fr_FR'
+STATIC_PATHS = ['pictures', 'documents']
+DEFAULT_PAGINATION = 10
+
+# Paths
+PATH = 'content'
+PAGE_PATHS = ['pages/fr']
+ARTICLE_PATHS = ['articles/fr']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,17 +31,46 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('Github', 'https://github.com/MathieuDepetris'),
           ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = False
 
 # Plugins
 PLUGIN_PATHS = ['C:\\ProgramData\\Anaconda3\\pkgs\\python-3.7.3-h8c8aaf0_1\\pelican-addon-clones\\pelican-plugins']
 PLUGINS = ['i18n_subsites', ]
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n'],}
 
+# i18n
+I18N_SUBSITES = {
+  'en': {
+    'PAGE_PATHS': ['pages/en'],
+    'ARTICLE_PATHS': ['articles/en'],
+    'LOCALE': 'en_UK'
+  }
+}
+I18N_GETTEXT_LOCALEDIR = 'pelican-fh5co-marble/locale/'
+I18N_GETTEXT_DOMAIN = 'messages'
+I18N_GETTEXT_NEWSTYLE = True
+I18N_TEMPLATES_LANG = 'fr_FR'
+
 # theme and theme localization
 THEME = 'pelican-fh5co-marble'
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+
+# Navigation and homepage options
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_PAGES_ON_HOME = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_TAGS_ON_MENU = False
+USE_FOLDER_AS_CATEGORY = True
+PAGE_ORDER_BY = 'order'
+
+# Contact section
+ABOUT = {
+  'image': '/images/about/objectif.png',
+  'mail': 'ob7@ird.fr',
+  # keep it a string if you dont need multiple languages
+  'text': 'Avenue Jean Monnet,<br/> CS 30171, 34203 Sète cedex, <br/>France',
+  #'link': 'contact.html',
+  # the address is also taken for google maps
+  'address': 'Avenue Jean Monnet,<br/> CS 30171, 34203 Sète cedex, <br/>France ',
+  'phone': '+33(0) 4 99 57 32 00'
+}
